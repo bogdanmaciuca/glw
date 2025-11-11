4ce33f5eb96494ce0557859adae0d16fea94196d
Bogdan-Alexandru Maciuca <bogdanmaciuca0621@gmail.com>
2025-11-12 00:24:39 +0200

    perspective function

--- include/glw/math.hpp
+++ include/glw/math.hpp
@@ -1,12 +1,31 @@
 #ifndef MATH_HPP
 #define MATH_HPP
 
-#include "glm/gtc/matrix_transform.hpp"
-
 #include "types.hpp"
 
 namespace glw {
+    /**
+    * @brief Constructs a view matrix
+    *
+    * @param [in] eye The position of the camera
+    * @param [in] point The point at which the camera looks
+    * @param [in] up The up vector of the camera
+    */
     mat4 LookAt(const vec3& eye, const vec3& point, const vec3& up = vec3(0, 1, 0));
+
+    /**
+    * @brief Constructs a projection matrix
+    * 
+    * @param [in] FOV Field of view in DEGREES
+    * @param [in] width The width of the render target
+    * @param [in] height The height of the render target
+    * @param [in] z_near
+    * @param [in] z_far
+    */
+    mat4 Perspective(
+        float FOV, float width, float height,
+        float z_near = 0.1f, float z_far = 100.0f
+    );
 }
 
 #endif // MATH_HPP
--- src/math.cpp
+++ src/math.cpp
@@ -1,8 +1,14 @@
 #include "glw/math.hpp"
 
+#include "glm/gtc/matrix_transform.hpp"
+
 namespace glw {
     mat4 LookAt(const vec3& eye, const vec3& point, const vec3& up) {
         return glm::lookAt(eye, point, up);
+    }
+
+    mat4 Perspective(float FOV, float width, float height, float z_near, float z_far) {
+        return glm::perspectiveFov(glm::radians(FOV), width, height, z_near, z_far);
     }
 }
 
